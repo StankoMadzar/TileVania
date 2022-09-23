078de640a2cc147efc735ee797498ea22cb90fff
Stanko Madzar <stanko.madzar@outlook.com>
2022-09-23 16:35:26 +0200

    Added basic LD + Rule tiles, Horizontal movement and animations

---
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody; 
+    Animator myAnimator;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    private void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y);
+      /*if(Math.Abs(moveInput.x) > Mathf.Epsilon)
+            myAnimator.SetBool("isRunning", true); 
+        else
+            myAnimator.SetBool("isRunning", false); >> still works, but not as elegant*/
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+        myRigidbody.velocity = playerVelocity;        
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+}
